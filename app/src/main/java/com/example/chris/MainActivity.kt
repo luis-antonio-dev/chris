2b6a6df364e25d87d01eafc69f381c52090a784c
Luis Antonio <luis.developer.ac@gmail.com>
2022-10-24 21:38:16 -0500

    initial commit

---
+++ app/src/main/java/com/example/chris/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.chris
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
